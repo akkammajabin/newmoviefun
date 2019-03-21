aea4da1bf9339b71165d63a70b1542a62df8f187
prasaks <akkamma_jabin@dell.com>
2019-03-21 12:27:40 +0530

    readfile

--- src/main/java/org/superbiz/moviefun/CsvUtils.java
+++ src/main/java/org/superbiz/moviefun/CsvUtils.java
@@ -2,10 +2,12 @@ package org.superbiz.moviefun;
 
 import com.fasterxml.jackson.databind.MappingIterator;
 import com.fasterxml.jackson.databind.ObjectReader;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -14,23 +16,26 @@ public class CsvUtils {
 
     public static String readFile(String path) {
         try {
-            Scanner scanner = new Scanner(new File(path)).useDelimiter("\\A");
+            //Scanner scanner = new Scanner(new File(path)).useDelimiter("\\A");
+            InputStream is = CsvUtils.class.getClassLoader().getResourceAsStream(path);
+            return convertStreamToString(is);
 
-            if (scanner.hasNext()) {
-                return scanner.next();
-            } else {
-                return "";
-            }
-
-        } catch (FileNotFoundException e) {
+        } catch (Exception e) {
+            e.printStackTrace();
             throw new RuntimeException(e);
         }
+    }
+
+    static String convertStreamToString(java.io.InputStream is) {
+        java.util.Scanner s = new java.util.Scanner(is).useDelimiter("\\A");
+        return s.hasNext() ? s.next() : "";
     }
 
     public static <T> List<T> readFromCsv(ObjectReader objectReader, String path) {
         try {
             List<T> results = new ArrayList<>();
 
+            System.out.println("Path >>>>>>>>>>"+path);
             MappingIterator<T> iterator = objectReader.readValues(readFile(path));
 
             while (iterator.hasNext()) {
@@ -39,6 +44,7 @@ public class CsvUtils {
 
             return results;
         } catch (IOException e) {
+            e.printStackTrace();
             throw new RuntimeException(e);
         }
     }
